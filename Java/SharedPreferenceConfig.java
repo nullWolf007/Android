8c1219ea907fd07c966b71f2d9c2b49db86b9965
黄楠 <1243065904@qq.com>
2018-01-22 13:16:34 +0800

    Add files via upload

---
+++ Java/SharedPreferenceConfig.java
@@ -0,0 +1,19 @@
+package com.inspeeding.ys400.common;
+
+/**
+ * Created by inspeeding_YF002 on 2017/12/13.
+ */
+
+public class SharedPreferenceConfig {
+
+    public static final String SHARED_CONFIG = "SHARED_CONFIG";
+    public static final String SHARED_TOEKN = "TOKEN";
+    public static final String SHARED_THRESHOLD = "THRESHOLD";
+
+    //2018.1.11 nullWolf
+    public static final String SHARED_PLCID = "SHARED_PLCID";//警员ID
+    public static final String SHARED_SECOND_PSW = "SHARED_SECOND_PSW";//权限密码
+    public static final String SHARED_DEVICE_ID = "SHARED_DEVICE_ID";//设备
+    public static final String SHARED_INTERVAL = "SHARED_INTERVAL";//间断时间
+
+}
